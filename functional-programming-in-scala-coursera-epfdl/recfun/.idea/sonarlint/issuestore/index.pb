33f109090811037cd5815e504fcbb28429d91e84
Derian Tungka <derian.tungka@traveloka.com>
2019-06-06 10:58:30 +0800

    Add solution to balance parentheses problem

--- functional-programming-in-scala-coursera-epfdl/recfun/.idea/sonarlint/issuestore/index.pb
+++ functional-programming-in-scala-coursera-epfdl/recfun/.idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,10 @@ W
 'src/test/scala/recfun/PascalSuite.scala,6/7/678cddd5c0f6c3c2149e39922f0634f2f2c9a5d7
 P
  src/main/scala/recfun/Main.scala,e/c/ecd5899f7817b03c9f64a68693025d559b3646dc
+9
+	build.sbt,7/4/74b95601c3a6d9d565cd312fac64094ec0f9d847
+>
+assignment.sbt,7/3/730375d2e85593daee77101b0bed7ff18bf694b9
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
--- functional-programming-in-scala-coursera-epfdl/recfun/src/main/scala/recfun/Main.scala
+++ functional-programming-in-scala-coursera-epfdl/recfun/src/main/scala/recfun/Main.scala
@@ -26,7 +26,38 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = {
+      def isBalanced(stack: List[Char], chars: List[Char]): Boolean = {
+        // if end of line is reached and stack is empty, return true
+        if (chars.isEmpty && stack.isEmpty) {
+          return true
+        } else if (chars.isEmpty && stack.nonEmpty) {
+          // else if end of line is reached and stack is still not empty, return false
+          return false
+        }
+        // else end of line not reached, so continue traversing
+        val curr = chars.head
+        val nextList = chars.tail
+
+        // if curr is ), check stack.
+        if (curr == ')') {
+          // if stack is empty (i.e. no floating parentheses), return false
+          if (stack.isEmpty) {
+            return false
+          } else if (stack.head == '(') { // if stack is non-empty, pop it and continue
+            isBalanced(stack.tail, nextList)
+          } else {
+            // if curr is any other character, just continue traversing
+            isBalanced(stack, nextList)
+          }
+        } else if (curr == '(') { // if curr is (, just add to stack and continue
+          isBalanced(curr +: stack, nextList)
+        } else { // if curr is any other character, just continue traversing
+          isBalanced(stack, nextList)
+        }
+      }
+      isBalanced(List.empty[Char],chars)
+    }
   
   /**
    * Exercise 3
